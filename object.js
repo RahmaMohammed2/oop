f3424cf692cdfd256f7425528f4264b4b9e19891
Rahma <rahmaurur@gmail.com>
2023-09-23 13:57:04 +0100

    my first commit

---
+++ object.js
@@ -0,0 +1,19 @@
+
+let player1 = {
+    firstName: "Gaashaan",
+    lastName: "Adan",
+    age: 13
+}
+
+let player2 = {
+    firstName: "Yonis",
+    lastName: "Yusuf",
+    age: 11
+}
+
+//console.log(player1.firstName)
+//console.log(player2.firstName)
+
+for(let i = 0; i<5; i++){
+    console.log("Rahma")
+}
